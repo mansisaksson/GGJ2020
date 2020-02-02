89bd20c941b6c608687e74310eb15c2b9bac9464
Måns Isaksson <mans.95.isaksson@gmail.com>
2020-02-02 10:37:47 +0100

    Text particles

--- js/link.js
+++ js/link.js
@@ -7,9 +7,9 @@ function createLinkAt(x, y, rotation, anchorElement) {
         textSize: { x: 0, y: 0 },
         draw: function () {
             ctx.save();
-            if(this.href.toLowerCase() == 'https://en.wikipedia.org/wiki/adolf_hitler') {
+            if (this.href.toLowerCase() == 'https://en.wikipedia.org/wiki/adolf_hitler') {
                 ctx.fillStyle = 'rgb(255, 60, 60)';
-                var text = drawTextAt(this.rigidBody.position.x, this.rigidBody.position.y, this.rigidBody.rotation, this.linkAscii, {fontSize: 64});
+                var text = drawTextAt(this.rigidBody.position.x, this.rigidBody.position.y, this.rigidBody.rotation, this.linkAscii, { fontSize: 64 });
             }
             else {
                 ctx.fillStyle = 'rgb(10, 70, 170)';
@@ -25,12 +25,24 @@ function createLinkAt(x, y, rotation, anchorElement) {
             let end = vecAdd(this.rigidBody.position, vecScalarMultiply(this.rigidBody.getRight(), this.textSize.x / 2));
             let hits = lineTrace(start, end, "bullet", 12)
             if (hits.length > 0) {
+                for (let i = 0; i < 25; i++) {
+                    let particleSpawnLocation = vecAdd(start, vecScalarMultiply(vecNormalize(vecSubtract(end, start)), lerp(0, vecLength(vecSubtract(end, start)), Math.random())));
+                    particles.push(createParticle(
+                        particleSpawnLocation.x,
+                        particleSpawnLocation.y,
+                        (Math.random() * 6.28),
+                        100 + Math.random() * 100,
+                        0.5 + Math.random() * 1,
+                        { r: 100, g: 140, b: 170 }
+                    ));
+                }
+
                 linkPortals.push(createLinkPortalAt(this));
                 destroyLinkByRigidBody(this.rigidBody);
                 destroyBulletByRigidBody(hits[0]);
                 let linkIndex;
-                for(let i = 0; i < links.length; i++) {
-                    if(links[i] == this) {
+                for (let i = 0; i < links.length; i++) {
+                    if (links[i] == this) {
                         linkIndex = i;
                         break;
                     }
